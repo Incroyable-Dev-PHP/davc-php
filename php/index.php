106528ae99584d4afc0666e55dee1852422cf382
davidcatty <davidcatty8@gmail.com>
2022-11-07 11:19:17 +0100

    completed

--- php/index.php
+++ php/index.php
@@ -1,16 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <!-- header included here -->
-    <!-- ... -->
+    <?php include './includes/head.html'; ?>
 
     <body>
-        <div class="container">
+        <?php
+            include './includes/header.html';
+            include './includes/nav.html';
+        ?>
+
+        <main class="container">
             <?php
-                // your code here with conditions
-                // ...
+                if (isset($_GET['about-me'])) {
+                    include './pages/about-me.html';
+                } elseif (isset($_GET['my-dreams'])) {
+                    include './pages/my-dreams.html';
+                } elseif (isset($_GET['my-passions'])) {
+                    include './pages/my-passions.html';
+                } else {
+                    include './pages/home.html';
+                }
             ?>
-        </div>
+        </main>
+
+        <?php include './includes/footer.html'; ?>
     </body>
 
 </html>
