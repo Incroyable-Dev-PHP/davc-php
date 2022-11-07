fa66a58ab6fb7ca8f7846d8ef716e7d3cc3ca0f2
davidcatty <davidcatty8@gmail.com>
2022-11-07 10:54:48 +0100

    first commit

---
+++ php/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <!-- header included here -->
+    <!-- ... -->
+
+    <body>
+        <div class="container">
+            <?php
+                // your code here with conditions
+                // ...
+            ?>
+        </div>
+    </body>
+
+</html>
